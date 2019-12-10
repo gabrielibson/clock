7aa88d74c8bcbb09e3c8e1632a7a309a75f57850
Gabriel Ibson <gabriel.ibson@ufpe.br>
2019-12-10 14:05:04 -0300

    Update data.sql to insert user only if it not already exists

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-insert into users (pis, username, password) values (123456789,'admin','YWRtaW46cGFzc3dvcmQ=');
+insert into users (pis, username, password)
+SELECT * FROM (SELECT 123456789, 'admin', 'YWRtaW46cGFzc3dvcmQ=') AS tmp
+WHERE NOT EXISTS (
+    SELECT pis FROM users WHERE pis = 123456789
+) LIMIT 1;
